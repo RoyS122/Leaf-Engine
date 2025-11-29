6c55e7ffb5c7dcd5cea7d43544bde56ea2bc054d
Roy Simon <roysimon.sco@gmail.com>
2025-11-30 00:14:08 +0100

    add: start adding functions on lua api

--- src/engine/lua_engine.c
+++ src/engine/lua_engine.c
@@ -1,9 +1,28 @@
 #include "lua_engine.h"
 #include "rooms.h"
-// Les includes de ton moteur sont déjà dans lua_engine.h
+#include "gameobjects.h"
 
-// Définition du pointeur global
 Game* Engine_game = NULL;
+
+
+
+int lua_create_gameobject(lua_State* L) {
+
+    const int room_id = lua_tointeger(L, 1);
+    if (Engine_game->RoomNBR <= room_id || room_id < 0) return lual_error(L, "Invalid room ID");
+    
+    const int go_id = Engine_game->RoomNBR;
+
+    GameObject *ngo = malloc(sizeof(GameObject));
+    if (!ngo) return luaL_error(L, "Failed to allocate GO");
+
+    if (init_gameobject(ngo)) return lual_error(L, "Failed to init gameobject");
+    ngo->free_obj = free_gameobject;
+
+    lua_pushinteger(L, go_id); // succès
+    return 1;
+}
+
 
 int lua_create_room(lua_State* L) {
     Room* nr = malloc(sizeof(Room));
@@ -11,7 +30,7 @@ int lua_create_room(lua_State* L) {
     init_room(nr);
     
     if (add_room(Engine_game, nr) != 0) {
-        free(nr); // libère la mémoire en cas d'erreur
+        free(nr); 
         lua_pushnil(L);                    
         lua_pushstring(L, "Error when adding room to game");
         return 2;                           
@@ -24,9 +43,9 @@ int lua_create_room(lua_State* L) {
 int lua_engine_log(lua_State* L) {
     
     const char* msg = lua_tostring(L, 1);
-        if (!msg) msg = "(null)";
-      printf("[Lua] %s\n", msg);
-      fflush(stdout);            // important sur Windows
+    if (!msg) msg = "(null)";
+    printf("[Lua] %s\n", msg);
+    fflush(stdout);            
     return 0;
 }
 
@@ -39,7 +58,7 @@ void register_engine_api(lua_State* L)
     // Ajouter Engine.create_room()
     lua_pushcfunction(L, lua_create_room);
     lua_setfield(L, -2, "create_room");
-    
+
     lua_pushcfunction(L, lua_engine_log);
     lua_setfield(L, -2, "log");
     
