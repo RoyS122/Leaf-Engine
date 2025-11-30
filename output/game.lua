a9dca00a3f87b28580970f612e23a8c94d0370dd
Roy Simon <roysimon.sco@gmail.com>
2025-11-30 16:38:06 +0100

    add: Premier draw called from lua file :)

--- output/game.lua
+++ output/game.lua
@@ -2,13 +2,17 @@ local Engine = Engine  -- récupère la table globale que tu as enregistrée en 
 
 
 
-local success, err = Engine.create_room()
-if success then 
+local room, err = Engine.create_room()
+if room then 
    Engine.log("test de success")
     print("test de success")
 end 
-if not success then
+if not room then
        Engine.log("test d'erreur")
 
     print("Failed:", err)
+end
+local player, err = Engine.create_gameobject(room, 10, 10)
+if Engine.setSprite(player, "./sprite_player_temp.png", 16, 16, 1, 1, 0) then
+    Engine.log("test d'erreur setSprite")
 end
