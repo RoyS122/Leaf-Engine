963f1f23e0530846ae3c1a1bc16886b68df05964
Roy Simon <roysimon.sco@gmail.com>
2025-12-02 18:09:13 +0100

    add: setStep for rooms

--- output/game.lua
+++ output/game.lua
@@ -13,11 +13,16 @@ if not room then
     print("Failed:", err)
 end
 
-local player, err = Engine.create_gameobject(room, 10, 10)
+local player, err = room:createGameObject(10, 10)
 if player:setSprite("./sprite_player_temp.png", 16, 16, 1, 1, 0) then
     Engine.log("test d'erreur setSprite")
 end
+
 player:setStep(function(self)
-    Engine.log("Test " .. self.x)
+    
     self.x = self.x + 1
+end)
+
+room:setStep(function(self)
+    Engine.log("Test room step")
 end)
