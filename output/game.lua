a2a5ef47d6f3dad5d1dede6bd0d6efca4ffd008e
Roy Simon <roysimon.sco@gmail.com>
2025-12-06 18:58:15 +0100

    add: camera system

--- output/game.lua
+++ output/game.lua
@@ -1,6 +1,4 @@
-local Engine = Engine  -- récupère la table globale que tu as enregistrée en C
-
-
+local Engine = Engine  -- get the global table engine
 
 local room, err = Engine.create_room()
 if room then 
@@ -13,16 +11,28 @@ if not room then
     print("Failed:", err)
 end
 
-local player, err = room:createGameObject(10, 10)
+local camera, err = room:createCamera(0, 0, 400, 300)
+
+Engine.log("test cam cam x: " .. camera.x)
+
+local enn_test, err = room:createGameObject(300, 10)
+local player, err = room:createGameObject(1, 10)
+
 if player:setSprite("./sprite_player_temp.png", 16, 16, 1, 1, 0) then
     Engine.log("test d'erreur setSprite")
 end
-
+if enn_test:setSprite("./sprite_player_temp.png", 16, 16, 1, 1, 0) then
+    Engine.log("test d'erreur setSprite")
+end
 player:setStep(function(self)
-    
-    self.x = self.x + 1
+    if not Engine.check_collision(enn_test, player) then
+        self.x = self.x + 1
+        -- camera.x = self.x - camera.w / 2
+        -- Engine.log("test cam cam x: " .. camera.x)
+    end 
 end)
 
 room:setStep(function(self)
     Engine.log("Test room step")
 end)
+
